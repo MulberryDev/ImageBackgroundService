3b62553e2e1812f472cbbfcaf511f8510a9a4698
NickSpicer <nickspicer@mulberry.com>
2015-09-09 14:22:57 +0100

    Add windows service

---
+++ FileBackgroundService/WindowsService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace FileBackgroundService
+{
+    class WindowsService : ServiceBase
+    {
+        public WindowsService()
+        {
+            this.ServiceName = "Image Background Service";
+            this.EventLog.Log = "Application";
+            this.CanHandlePowerEvent = true;
+            this.CanHandleSessionChangeEvent = true;
+            this.CanPauseAndContinue = true;
+            this.CanShutdown = true;
+            this.CanStop = true;
+        }
+
+        static void Main()
+        {
+            ServiceBase.Run(new WindowsService());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+        }
+
+        protected override void OnContinue()
+        {
+            base.OnContinue();
+        }
+
+        protected override void OnShutdown()
+        {
+            base.OnShutdown();
+        }
+
+        protected override void OnPowerEvent(PowerBroadcastStatus powerStatus)
+        {
+            base.OnPowerEvent(powerStatus);
+        }
+
+        protected override void OnSessionChange(SessionChangeDescription changeDescription)
+        {
+            base.OnSessionChange(changeDescription);
+        }
+    }
+}
